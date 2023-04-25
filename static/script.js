398288e9c6ee691db9b1a619ba81656018766f87
Anagha Radhakrishnan Moosad <anaghamoosad@gmail.com>
2023-04-25 14:50:28 +0530

    aligned the UI

--- static/script.js
+++ static/script.js
@@ -9,3 +9,77 @@ document.getElementById("generateplan").addEventListener("click", function() {
   loader.style.display = "block";
 
 })
+
+
+function generate() {
+  var doc = new jsPDF('l', 'pt');      
+  var res = doc.autoTableHtmlToJson(document.getElementById("meal-table"));     
+  var header = function(data) {
+    doc.setFontSize(25);
+    doc.setTextColor(40);
+    doc.setFontStyle('normal');
+    doc.text("Weekly Meal Plan", data.settings.margin.left, 50);
+  };
+  var options = {
+    beforePageContent: header,
+    margin: {
+      top: 100
+    },     
+    theme: 'grid',
+    columnStyles: {
+        0: {
+            
+           cellWidth: '80',
+            overflow: 'linebreak'
+                            
+            },
+        1: {
+          
+          cellWidth: '20',
+          overflow: 'linebreak'
+           
+           },
+        2: {
+           
+            cellWidth: '80',
+            overflow: 'linebreak'
+           
+           },
+        3: {
+             
+            cellWidth: '80' ,
+            overflow: 'linebreak'
+           },
+        4   : {
+             
+            cellWidth: '80',                 
+            overflow: 'linebreak'
+           },
+    },
+    rowStyles: {
+   
+      0: {
+        cellWidth: '80',
+        rowHeight: '80'
+        },
+    1: {
+      rowHeight: '80'
+       },
+    2: {
+      rowHeight: '80'
+       },
+    3: {
+      rowHeight: '80'
+       },
+    4   : {
+      rowHeight: '80'
+       }
+    },
+   
+  };
+
+  doc.autoTable(res.columns, res.data, options);
+ 
+
+  doc.save("weeklymealplan.pdf");
+}
